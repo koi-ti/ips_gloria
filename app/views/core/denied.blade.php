5c383e0e50e661ec4a6b581ef7e45b54e092dfa3
alforer <aforeror@gmail.com>
2019-04-05 10:11:43 -0500

    first commit

--- app/views/core/denied.blade.php
+++
@@ -1,9 +0,0 @@
-@extends ('core/layout')
-
-@section ('content')
-
-	<div class="alert alert-warning" role="alert">
-		No tienes suficientes permisos para acceder a esta página.
-	</div>
-
-@stop
--- app/views/core/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('core/layout')
-
-@section('content')
-
-<h1 class="page-header">Bienvenido</h1>
-<h4>{{ Auth::user()->nombre }}</h4>
-<span class="text-muted">{{ Auth::user()->email }}</span>
-
-@stop
